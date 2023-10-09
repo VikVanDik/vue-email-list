2e3ac17bc7c9d75988a0bd462efc0e272a65b60a
VikVanDik <95vlc17@gmail.com>
2023-10-09 17:16:09 +0200

    [add]: getMail functions

--- script/script.js
+++ script/script.js
@@ -4,7 +4,29 @@ const { createApp } = Vue;
 createApp({
   data () {
     return{
-
+      urlAPI : 'https://flynn.boolean.careers/exercises/api/random/mail'
     }
+  },
+
+  methods:{
+    emailStamp (){
+      axios.get(this.urlAPI)
+        .then((result)=>{
+          console.log(result.data.response)
+        })
+      },
+
+      mailLoop () {
+        for(let i = 0; i < 10; i++){
+          this.emailStamp()
+        }
+      }
+
+    },
+
+
+
+  mounted (){
+    this.mailLoop()
   }
-}) .mount('#app')
+}).mount('#app')
